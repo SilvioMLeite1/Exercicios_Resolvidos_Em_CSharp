e47214e5ef6018f4d651b0fecb7eb6f3ae1e4504
SilvioMLeite1 <smarioleite@outlook.com>
2020-06-25 01:47:22 -0300

    Problem Solution in C# - URI 1001

---
+++ URI_1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI_1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int x = a + b;
+
+            Console.WriteLine("X = " + x.ToString());
+        }
+    }
+}
